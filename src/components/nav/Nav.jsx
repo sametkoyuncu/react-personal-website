724ab9d8e06edd0e8a5b6b1ed4a5f4e021002112
sametkoyuncu <sametkoyuncu@live.com>
2022-05-03 11:33:07 +0300

    Add nav component

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import {useState} from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageSquareDetail} from 'react-icons/bi'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      {/* TODO: burayı temizle */}
+      <a href='#' className={activeNav === '#' ? 'active' : ''} onClick={()=> setActiveNav('#')}><AiOutlineHome /></a>
+      <a href='#about' className={activeNav === '#about' ? 'active' : ''} onClick={()=> setActiveNav('#about')}><AiOutlineUser /></a>
+      <a href='#experience' className={activeNav === '#experience' ? 'active' : ''} onClick={()=> setActiveNav('#experience')}><BiBook /></a>
+      <a href='#services' className={activeNav === '#services' ? 'active' : ''} onClick={()=> setActiveNav('#services')}><RiServiceLine /></a>
+      <a href='#contact' className={activeNav === '#contact' ? 'active' : ''} onClick={()=> setActiveNav('#contact')}><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
