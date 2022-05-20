a329ff0987465c601dbdb00b84b6c033ae71c3f3
sametkoyuncu <sametkoyuncu@live.com>
2022-05-20 11:20:42 +0300

    Add portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+  {
+    title: "This is a portfolio item title",
+    image: IMG1,
+    btnUrl1: "http://github.com",
+    btnUrl2: "http://github.com",
+  },
+   {
+    title: "This is a portfolio item title",
+    image: IMG2,
+    btnUrl1: "http://github.com",
+    btnUrl2: "http://github.com",
+  },
+    {
+    title: "This is a portfolio item title",
+    image: IMG3,
+    btnUrl1: "http://github.com",
+    btnUrl2: "http://github.com",
+  },
+     {
+    title: "This is a portfolio item title",
+    image: IMG4,
+    btnUrl1: "http://github.com",
+    btnUrl2: "http://github.com",
+  },
+      {
+    title: "This is a portfolio item title",
+    image: IMG5,
+    btnUrl1: "http://github.com",
+    btnUrl2: "http://github.com",
+  },
+       {
+    title: "This is a portfolio item title",
+    image: IMG6,
+    btnUrl1: "http://github.com",
+    btnUrl2: "http://github.com",
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {data.map(item => (
+          <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={item.image} alt="portfolio_image" />
+          </div>
+            <h3>{item.title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={item.btnUrl1} className='btn' target='_blank'>Github</a>
+              <a href={item.btnUrl2} className='btn btn-primary' target='_blank'>Live Demo</a>
+            </div>
+        </article>
+        ))}
+      </div>
+    </section>
   )
 }
 
